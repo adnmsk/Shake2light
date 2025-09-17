201c6de3920e05f92057795f56a64627a42a5155
AXD <axd@maaster.bg>
2025-09-17 12:10:58 +0300

    ver 2 with proximity

--- app/src/main/java/com/shake2light/MainActivity.kt
+++ app/src/main/java/com/shake2light/MainActivity.kt
@@ -15,7 +15,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.flashlightshake.R
+import com.shake2light.R
 
 class MainActivity : AppCompatActivity() {
 
